8da25053ccaaf04d45312f80374ec882ac85482d
Christian <gogochristian009@gmail.com>
2023-10-08 14:31:32 +0100

    mail template revu

--- resources/views/mail/invoice/paid.blade.php
+++ resources/views/mail/invoice/paid.blade.php
@@ -1,7 +1,5 @@
 <x-mail::message>
-<div class="text-center" style="text-align:center">
-    <img src="https://res.cloudinary.com/duk6hzmju/image/upload/v1696441954/digital-logo_micxsx.jpg" class="logo" alt="Laravel Logo">
-</div>
+
 <br>
 <h1 class="">{{$subject}}</h1> 
 <br>
