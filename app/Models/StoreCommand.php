d43ce7b98788144ae6bf3408c9da457eaabb5acc
Christian <gogochristian009@gmail.com>
2023-10-17 04:07:03 +0100

    ajout des commandes revus 42

--- app/Models/StoreCommand.php
+++ app/Models/StoreCommand.php
@@ -51,8 +51,8 @@ class StoreCommand extends Model
         return $this->belongsTo(UserSession::class, "session");
     }
 
-    function products(): BelongsTo
+    function products(): HasMany
     {
-        return $this->belongsTo(StoreProduit::class, "commands_products", "product_id", "command_id");
+        return $this->hasMany(StoreProduit::class, "commands_products", "product_id", "command_id");
     }
 }
