5843498a310fc4be8fb576bc4a719b15e4dd242c
Christian <gogochristian009@gmail.com>
2023-12-08 08:56:18 +0100

    affectation d'un agent à un store revue 2

--- app/Http/Controllers/Api/V1/StoreController.php
+++ app/Http/Controllers/Api/V1/StoreController.php
@@ -10,7 +10,7 @@ class StoreController extends STORE_HELPER
     public function __construct()
     {
         $this->middleware(['auth:api', 'scope:api-access']);
-        $this->middleware('CheckMasterOrAdmin')->except(['Stores', "RetrieveStore", "Stores"]);
+        $this->middleware('CheckMasterOrAdmin')->except(['Stores', "RetrieveStore", "Stores", "AffectToAgent"]);
         $this->middleware('CheckAgency')->only(["AffectToAgent"]);
     }
 
